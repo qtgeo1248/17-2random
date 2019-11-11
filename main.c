43bf7d2be7ef22deebc18b74473381b0f8d1e512
qtgeo1248 <gzhou00@stuy.edu>
2019-11-11 18:16:19 -0500

    populated array

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include "random.h"
 
 int main() {
-    printf("%d\n", ranNum());
+    int arr[10];
+    int i = 0;
+    printf("Generating Random Numbers:\n");
+    for (; i < 10; i++) {
+        int current = ranNum();
+        printf("\t%d\n", current);
+        arr[i] = current;
+    }
+
     return 0;
 }
